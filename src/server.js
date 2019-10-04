71313b996942b34b5e63ccb95bf435ea28a1cf4b
zxcvbnm <lhppaixaoo@gmail.com>
2019-10-04 20:51:24 -0300

    Finalização do projeto

--- src/server.js
+++ src/server.js
@@ -3,15 +3,35 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-g2h7j.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+});
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    
+    connectedUsers[user_id] = socket.id;
+
+});
+
+app.use((req, resp, next ) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+    
+})
 
 app.use(cors());
 app.use(express.json());
@@ -19,4 +39,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
